f3dc00b015bd502409a7c2797b81afa3664b0994
huybd-hblab <huybd@hblab.vn>
2025-07-12 15:43:11 +0700

    reconfig eslint

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -1,16 +1,3 @@
 import createConfig from "./create-config.js";
 
-export default {
-    env: {
-        es2021: true,
-        node: true,
-    },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 13,
-      sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {},
-}
+export default createConfig();
